901ab68f8728f297b1996350bd023eabee3a3ef4
adsieg01 <adsieg01@gmail.com>
2016-12-19 20:37:17 +0800

    Added Extra Credit Tasks
    Changes background color
    Extra Property
    Changes quote every 4 seconds

--- random-quote-v2/js/script.js
+++ random-quote-v2/js/script.js
@@ -1,42 +1,50 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
 // Quotes to be randomly generated
+
 var quotes = [
 	{	
 		key: "Quote One",
-		quote: "Love what you do and do what you love. Don't listen to anyone else who tells you not to do it. You do what you want, what you love. Imagination should be the center of your life.",
-		source: "Ray Bradbury",
-		citation: "http://www.goodreads.com/quotes/tag/follow-your-dreams",
-		year: "Unknown"
+		quote: "I learned the value of hard work by working hard.", 
+		source: "Margaret Mead",
+		citation: "www.brainyquote.com",
+		year: "",
+		tag: "History",
 	}, 
 	{
 		key: "Quote Two",
-		quote: "",
-		source: "",
-		citation: "",
-		year: ""
+		quote: "Nothing ever comes to one, that is worth having, except as a result of hard work.",
+		source: "Booker T. Washington",
+		citation: "www.brainyquote.com",
+		year: "",
+		tag: "History"
 	},
 	{
 		key: "Quote Three",
-		quote: "",
-		source: "",
-		citation: "",
-		year: ""
+		quote: "I’m a greater believer in luck, and I find the harder I work the more I have of it",
+		source: "Thomas Jefferson",
+		citation: "www.quotezine.com",
+		year: "",
+		tag: "Government"
 	},
 	{
 		key: "Quote Four",
-		quote: "",
-		source: "",
-		citation: "",
-		year: ""
+		quote: "Without labor nothing prospers.",
+		source: "Sophocles",
+		citation: "www.quotezine.com",
+		year: "",
+		tag: "History"
 	},
 	{
 		key: "Quote Five",
-		quote: "",
-		source: "",
-		citation: "",
-		year: ""
+		quote: "Dreams don’t work unless you do",
+		source: "John C. Maxwell",
+		citation:"",
+		year: "",
+		tag: "Inspirational"
+
 	}
 ];
 
@@ -44,7 +52,7 @@ var quotes = [
 
 function getRandomQuote () {
 	var randomNumber = Math.floor(Math.random() * quotes.length);
-	var selectedQuote = quotes[randomNumber].quote;
+	var selectedQuote = quotes[randomNumber];
 	return selectedQuote; 
 	}
 
@@ -52,20 +60,55 @@ function getRandomQuote () {
 
 	 
 
-// Displays Properties of the choosen quote
+// Displays Availible Properties of the choosen quote
 
 function printQuote () {
-	var postedQuote = getRandomQuote(); 
-	var info;
-	info += '<p class ="quote">' + postedQuote.quote  + '</p>';
-  info += '<p class ="source">' + postedQuote.source  + '</p>';
-  document.getElementById('quote-box').innerHTML = info;
+	var postedQuote = getRandomQuote ();
+	var info =  "";
+
+	  info += '<p class ="quote">' + postedQuote.quote  + '</p>';
+	  info += '<p class ="source">' + postedQuote.source;
+	  
+	  // additonal tag property 
+	  info += '<span class="tag">' + postedQuote.tag + '</span>';
+
+      	
+      	//posts citation and year only if applicable 
+      	
+      	if (postedQuote.citation != ""){
+      	info += '<span class="citation">' + postedQuote.citation + '</span>';
+ 		 }
+ 		 if(postedQuote.year != ""){
+	  	info += '<span class ="year">' + postedQuote.year  + '</span>';
+		 }
+  		
+  		// Writes the final quote to the page
+  		document.getElementById('quote-box').innerHTML = info;
 }
 
+// Change random background color of the page
+
+	function randomColor() { 
+                var hash = '#'; 
+                var colors = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0','FF69B4']; 
+                hash += colors[Math.floor(Math.random() * colors.length)];
+                document.getElementById('bg').style.background = hash; 
+            }
+
+			document.getElementById('loadQuote').addEventListener("click",randomColor, false);
+
+// Display every quote once before changing it
 
 
+		var i = 2
+		quotes.splice( i, Math.floor(Math.random() * quotes.length ))
+		console.log(quotes);
+	
 
-
+	
+// Changes the Quote and random color every four seconds
+	var quoteChange = window.setInterval(printQuote, 4000);
+	var colorChange = window.setInterval(randomColor, 4000);
 
 
 
